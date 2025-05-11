4f6b5e1ff6707b9238cf9c48d5e494327ba4bc16
Vinicius Yudi <vinizzly2005@gmail.com>
2025-05-11 00:37:24 -0300

    Tela Cadastro Instituiçao
    Não esta 100% ainda

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
 import Institutions from "./pages/Institutions"
+import InstitutionRegister from "./pages/InstitutionRegister";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/Register" element={<Register />} />
         <Route path="/Institutions" element={<Institutions />} />
+        <Route path="/InstitutionRegister" element={<InstitutionRegister />} /> {/* Substitua pelo componente correto */}
         <Route path="/" element={<Home />} /> {/* Redireciona para Login por padrão */}
 
       </Routes>
